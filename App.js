3df91a62ca1ac6e02ddac06fc94aca716dc5d580
cuong2002k <masenkogl@gmail.com>
2024-04-07 10:51:28 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react, { useState } from "react";
+import { Dimensions, Image, ImageBackground, SafeAreaView, StyleSheet, Text } from "react-native";
+import { TextInput } from "react-native";
+import { Button, View } from "react-native";
+import { TouchableOpacity } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [userName, onChangeUserName] = useState("");
+  const [password, onChangePassword] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ImageBackground style={styles.background} source={require("./assets/background.png")}>
+        <Image source={require("./assets/logo.png")} style={styles.image} />
+        <Text style={styles.text}>
+          REGISTER
+        </Text>
+        <TextInput
+          style={styles.input}
+          placeholder="USERNAME"
+          value={userName}
+          onChangeText={onChangeUserName}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="PASSWORD"
+          value={password}
+          secureTextEntry
+          onChangeText={onChangePassword} />
+
+        <TouchableOpacity style={styles.button}
+          onPress={() => {
+            alert(`Hello ${userName} ${password}`);
+            onChangeUserName("");
+            onChangePassword("");
+          }}
+        >
+          <Text>
+            Login
+          </Text>
+        </TouchableOpacity>
+      </ImageBackground>
+    </SafeAreaView>
+
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+
   },
+  background: {
+    flex: 1,
+    resizeMode: 'center',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: "center",
+  },
+  image: {
+    width: 300,
+    height: 200
+  },
+  button: {
+    backgroundColor: "aqua",
+    color: 'white',
+    padding: 10,
+    width: 200,
+    marginTop: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  input: {
+    height: 40,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10,
+    color: "black",
+    marginTop: 10
+  },
+  text: {
+    color: "black",
+  }
 });
+export default App;
+
